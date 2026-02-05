ef2fd44ce8e286fb1ddb9131bcd9634ed0a3cacf
Nilton <aluno24823@mtorga.edu.pt>
2026-02-05 23:20:17 +0000

    primeiro commit

---
+++ dateType.py
@@ -0,0 +1,64 @@
+#Type string(regiao)
+
+Norte = "Norte"
+Centro = "Centro"
+Area_Metropolitana_de_Lisboa = "Área Metropolitana de Lisboa"
+Alentejo = "Alentejo"
+Algarve = "Alagarve"
+Açores = "Açores"
+Madeira = "Madeira"
+print(type(Norte))
+print(type(Centro))
+print(type(Area_Metropolitana_de_Lisboa))
+print(type(Alentejo))
+print(type(Algarve))
+print(type(Açores))
+print(type(Madeira))
+
+#Type int(habitantes)
+norte_habitantes = 3574394
+centro_habitantes = 2223958
+Area_Metropolitana_de_Lisboa_habitantes = 284006
+Alentejo_habitantes = 708714
+Algarve_habitantes = 439241
+acores_habitantes = 236657
+Madeira_habitantes = 259440
+print(type(norte_habitantes))
+print(type(centro_habitantes))
+print(type(Area_Metropolitana_de_Lisboa_habitantes))
+print(type(Alentejo_habitantes))
+print(type(Algarve_habitantes))
+print(type(acores_habitantes))
+print(type(Madeira_habitantes))
+
+#Type float (Área)
+norte_area = 21.285
+centro_area = 28.199
+Area_Metropolitana_de_Lisboa_area = 2.840
+Alentejo_area = 31.604
+Algarve_area = 4.996
+Açores_area = 2.322
+Madeira_area = 741
+print(type(norte_area))
+print(type(centro_area))
+print(type(Area_Metropolitana_de_Lisboa_area))
+print(type(Alentejo_area))
+print(type(Algarve_area))
+print(type(Açores_area))
+print(type(Madeira_area))
+
+#type bool (regiao costeira)
+norte_costeira = True
+centro_costeira = True
+area_metropolitana_de_lisboa_costeira = True
+alentejo_costeira = True
+algarve_costeira = True
+acores_costeira = True
+madeira_costeira = True
+print(type(norte_costeira))
+print(type(centro_costeira))
+print(type(area_metropolitana_de_lisboa_costeira))
+print(type(alentejo_costeira))
+print(type(algarve_costeira))
+print(type(acores_costeira))
+print(type(madeira_costeira))
